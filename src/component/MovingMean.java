141043f53fff70b876d4a4e1e2e601b923579023
Emmanuel Douge <emmdouge@gmail.com>
2018-03-03 16:19:46 -0500

    fixed moving mean

--- src/component/MovingMean.java
+++ src/component/MovingMean.java
@@ -1,4 +1,5 @@
 package component;
+
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -7,133 +8,140 @@ import org.jfree.ui.RefineryUtilities;
 import reader.Data;
 
 public class MovingMean extends GroupingChart {
-	
+
 	private double processMean;
 	private double avgRange;
-	
+
 	public MovingMean(Data d, double avgRange) throws Exception {
 		this(d, 3, avgRange);
 	}
-	
+
 	public MovingMean(Data d, int k, double avgRange) throws Exception {
-		this.avgRange = avgRange;
-		this.data = d;
-		this.sampleSize = k-1;
-		d.setSampleSize(this.sampleSize);
-		this.offset = k-1;
-		if(d.getUseCols()) {
-			d.setType("Moving Means k = "+k);
+		if (d.getUseCols()) {
+			this.avgRange = avgRange;
+			this.data = d;
+			this.sampleSize = k;
+			d.setSampleSize(this.sampleSize - 1);
+			this.offset = k - 1;
+			d.setType("Moving Means k = " + k);
 			ArrayList<Integer> colOffsets = new ArrayList<Integer>();
 			ArrayList<double[]> allLines = new ArrayList<double[]>();
-			for(int x = 0; x < d.getPointsPerRow(); x++) {
-				colOffsets.add(d.getColOffsets().get(x)+this.sampleSize);
-				this.numSamples = data.getCols().get(x).length-this.sampleSize;
+			// iterate through column
+			for (int x = 0; x < d.getPointsPerRow(); x++) {
+				colOffsets.add(d.getColOffsets().get(x));
+				this.numSamples = data.getCols().get(x).length - this.sampleSize + 1;
 				System.out.println(this.numSamples);
-				System.out.println("sample size: "+this.sampleSize);
-				System.out.println("num samples: "+this.numSamples);
+				System.out.println("sample size: " + this.sampleSize);
+				System.out.println("num samples: " + this.numSamples);
 				double[] points = new double[this.numSamples];
-				for(int i = 0; i < this.numSamples; i++) {
+				for (int i = 0; i < this.numSamples; i++) {
 					int start = i;
-					int end = (i)+this.sampleSize;
+					int end = (i) + this.sampleSize;
 					double mean = calcPoints(data.getCols().get(x), start, end);
 					points[i] = mean;
-	//				this.processMean += points[i];
-	//				System.out.println(i+" mm: "+mean+" ("+start+"~"+end+")"+" +avg: "+this.processMean);
+					this.processMean += points[i];
+					System.out.println(
+							i + " mm: " + mean + " (" + start + "~" + (end - 1) + ")" + " +avg: " + this.processMean);
 				}
-				this.processMean = this.processMean/this.numSamples;
-				System.out.println("process mean: "+this.processMean);
+				this.processMean = this.processMean / this.numSamples;
+				System.out.println("process mean: " + this.processMean);
 				allLines.add(points);
 			}
 			d.setColOffsets(colOffsets);
 			limits = new ArrayList<Double>();
 			XYSeriesChart.run(d, allLines, limits, d.getColOffsets());
-		}
-		else {
-			d.setType("Moving Mean k ="+k);
+		} else {
+			this.avgRange = avgRange;
+			this.data = d;
+			this.sampleSize = k;
+			d.setSampleSize(this.sampleSize);
+			this.offset = k - 1;
+			d.setType("Moving Mean k =" + k);
 			yNames = new ArrayList<String>();
-			yNames.add("Mean k = "+k);
+			yNames.add("Mean k = " + k);
 			d.setYNames(yNames);
-			this.numSamples = data.getAllPoints().length-this.sampleSize;
+			this.numSamples = data.getAllPoints().length - this.sampleSize + 1;
 			System.out.println(this.numSamples);
-			System.out.println("sample size: "+this.sampleSize);
-			System.out.println("num samples: "+this.numSamples);
+			System.out.println("sample size: " + this.sampleSize);
+			System.out.println("num samples: " + this.numSamples);
 			points = new double[this.numSamples];
-			for(int i = 0; i < this.numSamples; i++) {
+			for (int i = 0; i < this.numSamples; i++) {
 				int start = i;
-				int end = (i)+this.sampleSize;
+				int end = (i) + this.sampleSize;
 				double mean = calcPoints(d.getAllPoints(), start, end);
 				points[i] = mean;
 				this.processMean += points[i];
-				System.out.println(i+" mm: "+mean+" ("+start+"~"+end+")"+" +avg: "+this.processMean);
+				System.out.println(
+						i + " mm: " + mean + " (" + start + "~" + (end - 1) + ")" + " +avg: " + this.processMean);
 			}
-			this.processMean = this.processMean/this.numSamples;
-			System.out.println("process mean: "+this.processMean);
+			this.processMean = this.processMean / this.numSamples;
+			System.out.println("process mean: " + this.processMean);
 			limits = calcLimits(k);
 			Collections.sort(limits);
 			ArrayList<double[]> allLines = new ArrayList<double[]>();
 			allLines.add(points);
 			ArrayList<Integer> offsets = new ArrayList<Integer>();
 			offsets.add(this.offset);
-			
+
 			XYSeriesChart.run(d, allLines, limits, offsets);
 		}
 	}
-	
+
 	/**
 	 * Calculates the mean for each sample
 	 */
 	public double calcPoints(double[] data, int start, int end) {
 		double sum = 0;
-		for(int i = start; i < end; i++) {
+		for (int i = start; i < end; i++) {
 			sum += data[i];
 		}
-		return sum/(end-start);
+		return sum / (end - start);
 	}
-	
+
 	protected ArrayList<Double> calcLimits(int k) throws Exception {
 		ArrayList<Double> limits = new ArrayList<Double>();
 		double a2 = 0;
 		switch (this.sampleSize) {
-			case 1:
-				throw new Exception("SAMPLE SIZE TOO SMALL!");
-			case 2: 
-				a2 = 1.88;
-				break;
-			case 3: 
-				a2 = 1.02;
-				break;
-			case 4: 
-				a2 = .73;
-				break;
-			case 5: 
-				a2 = .58;
-				break;
-			case 6: 
-				a2 = .48;
-				break;
-			case 7: 
-				a2 = .42;
-				break;
-			case 8: 
-				a2 = .37;
-				break;
-			case 9: 
-				a2 = .34;
-				break;
-			case 10: 
-				a2 = .31;
-				break;
-			case  11: 
-				a2 = .29;
-				break;
-			default: 
-				a2 = .27;
-				break;
+		case 1:
+			throw new Exception("SAMPLE SIZE TOO SMALL!");
+		case 2:
+			a2 = 1.88;
+			break;
+		case 3:
+			a2 = 1.02;
+			break;
+		case 4:
+			a2 = .73;
+			break;
+		case 5:
+			a2 = .58;
+			break;
+		case 6:
+			a2 = .48;
+			break;
+		case 7:
+			a2 = .42;
+			break;
+		case 8:
+			a2 = .37;
+			break;
+		case 9:
+			a2 = .34;
+			break;
+		case 10:
+			a2 = .31;
+			break;
+		case 11:
+			a2 = .29;
+			break;
+		default:
+			a2 = .27;
+			break;
 		}
-		limits.add(this.processMean+((2.0/3.0)*this.avgRange*a2));
-		limits.add(this.processMean-((2.0/3.0)*this.avgRange*a2));
-		limits.add(this.processMean+(this.avgRange*a2));
-		limits.add(this.processMean-(this.avgRange*a2));
+		limits.add(this.processMean + ((2.0 / 3.0) * this.avgRange * a2));
+		limits.add(this.processMean - ((2.0 / 3.0) * this.avgRange * a2));
+		limits.add(this.processMean + (this.avgRange * a2));
+		limits.add(this.processMean - (this.avgRange * a2));
 		return limits;
 	}
 }
--- test/component/TestMovingMean.java
+++ test/component/TestMovingMean.java
@@ -25,11 +25,15 @@ public class TestMovingMean {
 	Data shiftData;
 	Data newTestsPassingData;
 	Data cyclo;
+	Data periodData;
+	Data periodColData;
 	
 	@Before
 	public void setup() throws Exception {
 		shiftData = TxtFileReader.readFile(Constant.TEST_SHIFT);
 		newTestsPassingData = TxtFileReader.readFile(Constant.NEW_TESTS_PASSING);
+		periodData = TxtFileReader.readFile("period");
+		periodColData = TxtFileReader.readFile("periodcols", true);
 		cyclo = TxtFileReader.readFile(Constant.PACKAGE_AVG_CYCLO_COMPLEXITY, true);
 	}
 	
@@ -48,6 +52,25 @@ public class TestMovingMean {
 		MovingRange r = new MovingRange(shiftData, k);
 		MovingMean mm = new MovingMean(shiftData, k, r.getAvgRange());
 		assertEquals(212, mm.getPoints().length);
+		assertEquals(0, JOptionPane.showConfirmDialog(null, "Does this look right?", "TEST", JOptionPane.YES_NO_OPTION));
+	}	
+
+	@Test
+	public void testPeriodData() throws Exception {
+		int k = 3;
+		MovingRange r = new MovingRange(periodData, k);
+		MovingMean mm = new MovingMean(periodData, k, r.getAvgRange());
+		assertEquals(10, mm.getPoints().length);
+		assertEquals(2483.333, mm.points[0], .001);
+		assertEquals(2480.666, mm.points[mm.points.length-1], .001);
+		assertEquals(0, JOptionPane.showConfirmDialog(null, "Does this look right?", "TEST", JOptionPane.YES_NO_OPTION));
+	}	
+
+	@Test
+	public void testPeriodColData() throws Exception {
+		int k = 3;
+		MovingRange r = new MovingRange(periodColData, k);
+		MovingMean mm = new MovingMean(periodColData, k, 0);
 		assertEquals(0, JOptionPane.showConfirmDialog(null, "Does this look right?", "TEST", JOptionPane.YES_NO_OPTION));
 	}	
 	
